64a829d3030211b94aa6c250378f6c56379132ea
mika1315 <g1720505@is.ocha.ac.jp>
2019-06-21 15:48:09 +0900

    Googleから一番遠いリンクを見つけるプログラム

--- wikipedia/homework4_wikipedia.py
+++ wikipedia/homework4_wikipedia.py
@@ -18,9 +18,12 @@ def nicknames_from_file():
     return persons, nameToID
 
 
-def nameToID_from_dict(name, persons, nameToID):
-    rootID = nameToID[name]
-    return rootID
+def nameToID_from_dict(name, nameToID):
+    if name in nameToID:
+        rootID = nameToID[name]
+        return rootID
+    else:
+        print("Not found")
 
 
 def links_from_file():
@@ -46,7 +49,7 @@ def bfs(originID, destinationID):
     visited = set()
     queue = collections.deque()
 
-    step_dict = dict() # ステップ数を格納
+    # step_dict = dict() # ステップ数を格納
     preID_dict = dict() # ルートを格納
 
     queue.append(originID)  # 現在地を探索候補キューに格納
@@ -72,9 +75,41 @@ def bfs(originID, destinationID):
                 queue.append(neighbor)
             
     return -1, []
+
+
+def bfsToEnd(originID):
+    visited = []
+    queue = collections.deque()
+
+    # step_dict = dict() # ステップ数を格納
+    preID_dict = dict() # ルートを格納
+
+    queue.append(originID)  # 現在地を探索候補キューに格納
+    visited.append(originID) # 現在地を探索済リストに格納
+    preID_dict[originID] = [originID] # 出発地を格納
+
+    links = links_from_file()
+
+    # キューが空になるまで
+    while queue:
+
+        vertexID = queue.popleft() # キューから次の探索地点を一つ取り出す 
+        # visited.add(vertexID) # 「探索済みリスト」に取り出した地点を格納（ここが現在地点）
+
+        for neighbor in links_from_dict(vertexID, links): # 現在地から次に行けるポイントを調べる
+
+            if neighbor not in visited:
+                preID_dict[neighbor] = preID_dict[vertexID] + [neighbor]
+                visited.append(neighbor)
+                queue.append(neighbor)
+    
+    thelongestID = vertexID
+            
+    return thelongestID
                 
+
 def print_route(step, route):
-    persons, nameToID = znicknames_from_file()
+    persons, nameToID = nicknames_from_file()
 
     if step != -1: # 目的地まで到着できたらルートを表示
         for person in route:
@@ -83,15 +118,27 @@ def print_route(step, route):
         print('Fail!')
 
 
+def print_shortest_route():
+    while True:
+        originName = input("origin : ")
+        destinationName = input("destination : ")
+        persons, nameToID = nicknames_from_file() # nicknames.txt から辞書を生成
+        originID = nameToID_from_dict(originName, nameToID) # 出発地の名前からIDを返す
+        destinationID = nameToID_from_dict(destinationName, nameToID) # 目的地の名前からIDを返す
+        step, route = bfs(originID, destinationID) # 出発地から目的地まで探索する
+
+        print(originName, " >> ", destinationName)
+        print(step, "step")
+        print_route(step, route) # ルートを表示
+    return None
+
+
 if __name__ == "__main__":
-    originName = "お茶の水女子大学" # 出発地
-    destinationName = "OCaml" # 目的地
+    # print_shortest_route()
+    originName = "Google"
     persons, nameToID = nicknames_from_file() # nicknames.txt から辞書を生成
-    originID = nameToID_from_dict(originName, persons, nameToID) # 出発地の名前からIDを返す
-    destinationID = nameToID_from_dict(destinationName, persons, nameToID) # 目的地の名前からIDを返す
-    step, route = bfs(originID, destinationID) # 出発地から目的地まで探索する
+    originID = nameToID_from_dict(originName, nameToID) # 出発地の名前からIDを返す
+    print(persons[bfsToEnd(originID)])
+    print(bfsToEnd(originID))
 
-    print(originName, " >> ", destinationName)
-    print(step, "step")
-    print_route(step, route) # ルートを表示
-
+    
